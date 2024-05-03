1e1c1031711d4586ebf786ec38fb71c5e70d2ccb
sophiamersmann <sophia.mersmann1@gmail.com>
2024-05-03 07:32:31 +0200

    Detect URL from clipboard

--- owid/src/open-staging-site.tsx
+++ owid/src/open-staging-site.tsx
@@ -1,5 +1,16 @@
-import { List, ActionPanel, Action, Icon, open, Keyboard } from "@raycast/api";
-import { usePullRequests, makeGrapherURL, linkIcon } from "./utils";
+import {
+  List,
+  ActionPanel,
+  Action,
+  Icon,
+  open,
+  Keyboard,
+  showToast,
+  Toast,
+  Clipboard,
+} from "@raycast/api";
+import { usePullRequests, linkIcon } from "./utils";
+import { usePromise } from "@raycast/utils";
 
 const GITHUB_REPO = "owid/owid-grapher";
 const GITHUB_USER_NAME = "sophiamersmann";
@@ -9,20 +20,40 @@ const BROWSER_NAME = "Google Chrome";
 
 const ARC_PATH = "/Applications/Arc.app";
 
-const EXAMPLE_CHART_SLUG = "life-expectancy";
-
 export default function Command() {
-  const { pullRequests, isLoading } = usePullRequests(
+  const { data: clipboardText = "", isLoading: isLoadingClipboardText } =
+    usePromise(Clipboard.readText, [], {
+      onError: async () => {
+        showToast(Toast.Style.Failure, "Failed to read clipboard content");
+      },
+    });
+
+  const { pullRequests, isLoading: isLoadingPullRequests } = usePullRequests(
     GITHUB_REPO,
     GITHUB_USER_NAME,
   );
+
+  const maybeUrl = clipboardText.trim();
+
+  let urlPath = "";
+
+  try {
+    const url = new URL(maybeUrl);
+    urlPath = url.pathname + url.search;
+  } catch (error) {
+    // intentionally empty
+  }
+
+  const makeUrl = (baseUrl: string) => baseUrl + urlPath;
+
+  const isLoading = isLoadingClipboardText || isLoadingPullRequests;
 
   return (
     <List isLoading={isLoading}>
       {pullRequests.map((pr) => (
         <List.Item
           key={pr.branch}
-          title={pr.branch}
+          title={makeUrl(pr.staging)}
           icon={linkIcon}
           accessories={[{ date: pr.updatedAt }]}
           actions={
@@ -30,16 +61,26 @@ export default function Command() {
               <Action
                 title={`Open in ${BROWSER_NAME}`}
                 icon={Icon.Globe}
-                onAction={() => open(pr.staging, BROWSER_PATH)}
+                onAction={() => open(makeUrl(pr.staging), BROWSER_PATH)}
               />
               <Action
                 title="Open in Little Arc"
+                icon={Icon.Globe}
+                onAction={() => open(makeUrl(pr.staging), ARC_PATH)}
+              />
+              <Action
+                title={`Open Homepage in ${BROWSER_NAME}`}
+                icon={Icon.Globe}
+                onAction={() => open(pr.staging, BROWSER_NAME)}
+              />
+              <Action
+                title={`Open Homepage in Little Arc`}
                 icon={Icon.Globe}
                 onAction={() => open(pr.staging, ARC_PATH)}
               />
               <Action.CopyToClipboard
                 title="Copy Link"
-                content={pr.staging}
+                content={makeUrl(pr.staging)}
                 shortcut={Keyboard.Shortcut.Common.Copy}
               />
             </ActionPanel>
