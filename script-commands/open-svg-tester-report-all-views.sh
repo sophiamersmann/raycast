c78699271e7fe9391c99af761f44322988bf548b
sophiamersmann <sophia.mersmann1@gmail.com>
2024-06-07 23:22:42 +0200

    Add script commands

---
+++ script-commands/open-svg-tester-report-all-views.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# Required parameters:
+# @raycast.schemaVersion 1
+# @raycast.title Open SVG Tester Report (All Views)
+# @raycast.mode compact
+
+# Optional parameters:
+# @raycast.icon 🧪
+# @raycast.argument1 { "type": "text", "placeholder": "Branch" }
+
+# Documentation:
+# @raycast.author sophiamersmann
+# @raycast.authorURL https://raycast.com/sophiamersmann
+
+cd ~/code/owid/owid-grapher-svgs
+
+git fetch && git checkout -f $1 && git reset --hard origin/$1 && git clean -fd
+
+if test -f report_all-views.html; then
+    open -a Google\ Chrome\ Dev.app report_all-views.html
+    echo "Report opened in Chrome Dev"
+else
+    echo "No report found"
+fi
